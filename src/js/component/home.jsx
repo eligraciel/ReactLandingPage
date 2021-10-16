c70d9e46cc6548c09dfd96a43a13320180a3c798
eligraciel <eliana.bravo@gmail.com>
2021-10-16 03:13:07 +0000

    primer eenvio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,102 @@
 import React from "react";
+import "bootstrap";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//componente navegador
 
 //create your first component
-const Home = () => {
+export const Navegador = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+			<div className="mx-5">
+				<a className="navbar-brand" href="#">
+					Start Bootstrat
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+			</div>
+
+			<div
+				className="collapse navbar-collapse d-flex flex-row-reverse"
+				id="navbarSupportedContent">
+				<ul className="navbar-nav mr-auto mx-5">
+					<li className="nav-item active">
+						<a className="nav-link" href="#">
+							Home <span className="sr-only">(current)</span>
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							About
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Service
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export const Jumbotrom = () => {
+	return (
+		<div className="jumbotron mx-5 bg-light py-3">
+			<h1 className="display-4">A Warm Welcome!</h1>
+			<p className="lead text-wrap">
+				This is a simple hero unit, a simple jumbotron-style component
+				for calling extra attention to featured content or information.
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+
+			<p className="lead">
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Learn more
+				</a>
 			</p>
 		</div>
 	);
 };
 
-export default Home;
+export const Tarjetas = ({ imagen, titulo, texto }) => {
+	return (
+		<div className="card">
+			<img className="card-img-top" src={imagen} alt="Card image cap" />
+			<div className="card-body d-flex flex-column">
+				<h5 className="card-title">{titulo}</h5>
+				<p className="card-text text-wrap">{texto}</p>
+				<a href="#" className="btn btn-primary mt-auto">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export const Pie = () => {
+	return (
+		<div className="bg-dark py-2 text-light">
+			<p className="text-light text-center">CopyRight ° Página</p>
+		</div>
+	);
+};
+
+Tarjetas.propTypes = {
+	imagen: PropTypes.string,
+	titulo: PropTypes.string,
+	texto: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,41 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Navegador, Jumbotrom, Tarjetas, Pie } from "./component/home.jsx";
+
+const Construir = () => {
+	return (
+		<>
+			<Navegador />
+			<div className="container w-65">
+				<Jumbotrom />
+				<div className="d-flex justify-content-between mx-5">
+					<Tarjetas
+						imagen="https://picsum.photos/id/879/500/325"
+						titulo="Prueba1"
+						texto="Lorem ipsum dolor sit amet, consectetur adipiscing e"
+					/>
+					<Tarjetas
+						imagen="https://picsum.photos/id/356/500/325"
+						titulo="Prueba2"
+						texto="Sed ut perspiciatis unde omnis iste natus erro"
+					/>
+					<Tarjetas
+						imagen="https://picsum.photos/id/100/500/325"
+						titulo="Prueba3"
+						texto="Atae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia volupta"
+					/>
+					<Tarjetas
+						imagen="https://picsum.photos/id/234/500/325"
+						titulo="Prueba4"
+						texto="Cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quo"
+					/>
+				</div>
+			</div>
+			<Pie />
+		</>
+	);
+};
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Construir />, document.querySelector("#app"));
